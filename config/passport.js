2689d207a73327d29959b6bdf19a3a0dd68ac436
caesar <newman0934@gmail.com>
2019-10-18 16:20:35 +0800

    login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
-const GoogleStrategy = require("passport-google-oauth20").Strategy
 const bcrypt = require("bcryptjs");
 
 const db = require("../models")
@@ -74,53 +73,16 @@ module.exports = passport => {
     )
   );
 
-  passport.use(
-    new GoogleStrategy({
-        clientID: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK,
-        profileFields: ["email", "displayName"]
-      },
-      (accessToken, refreshToken, profile, done) => {
-        User.findOne({
-          where: {email: profile._json.email}
-        }).then(user => {
-          if (!user) {
-            let randomPassword = Math.random()
-              .toString(36)
-              .slice(-8);
-            bcrypt.genSalt(10, (err, salt) => {
-              bcrypt.hash(randomPassword, salt, (err, hash) => {
-                let newUser = User({
-                  name: profile._json.name,
-                  email: profile._json.email,
-                  password: hash
-                });
-                newUser
-                  .save()
-                  .then(user => {
-                    return done(null, user);
-                  })
-                  .catch(err => {
-                    console.log(err);
-                  });
-              });
-            });
-          } else {
-            return done(null, user);
-          }
-        });
-      }
-    )
-  );
+  
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
   passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-      done(err, user);
-    });
-  });
-};
+    User.findByPk(id)
+      .then((user) => {
+        done(null, user)
+      })
+  })
+}
